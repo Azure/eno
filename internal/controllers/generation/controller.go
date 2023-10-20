e77cabbd5548ed431a173930af290c9f00263c4e
Jordan Olshevski <jordan.olshevski@microsoft.com>
2023-10-20 13:19:47 -0500

    Switch to pods for generation

--- internal/controllers/generation/controller.go
+++ internal/controllers/generation/controller.go
@@ -8,7 +8,6 @@ import (
 	"strconv"
 
 	"github.com/go-logr/logr"
-	batchv1 "k8s.io/api/batch/v1"
 	corev1 "k8s.io/api/core/v1"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
@@ -33,7 +32,7 @@ func NewController(mgr ctrl.Manager, config *conf.Config) error {
 
 	_, err := ctrl.NewControllerManagedBy(mgr).
 		For(&apiv1.Composition{}).
-		Owns(&batchv1.Job{}).
+		Owns(&corev1.Pod{}).
 		Build(c)
 
 	return err
@@ -54,17 +53,24 @@ func (c *Controller) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Resu
 	gen.Name = comp.Spec.Generator.Name
 	err = c.client.Get(ctx, client.ObjectKeyFromObject(gen), gen)
 	if err != nil {
-		return ctrl.Result{}, fmt.Errorf("listing current jobs: %w", err)
+		return ctrl.Result{}, fmt.Errorf("listing current pods: %w", err)
 	}
 
-	// Avoid creating duplicate jobs
-	job := c.newJob(comp, gen)
-	current := &batchv1.Job{}
-	err = c.client.Get(ctx, client.ObjectKeyFromObject(job), current)
+	// Avoid creating duplicate pods
+	pod := c.newPod(comp, gen)
+	current := &corev1.Pod{}
+	err = c.client.Get(ctx, client.ObjectKeyFromObject(pod), current)
 	if client.IgnoreNotFound(err) != nil {
-		return ctrl.Result{}, fmt.Errorf("listing current jobs: %w", err)
+		return ctrl.Result{}, fmt.Errorf("getting current pod: %w", err)
 	}
-	if err == nil { // !404
+	if err == nil {
+		if current.DeletionTimestamp == nil && shouldDeletePod(current) {
+			err = c.client.Delete(ctx, current)
+			if err != nil {
+				return ctrl.Result{}, fmt.Errorf("error deleting pod: %w", err)
+			}
+			c.logger.Info("deleted generation pod", "podName", current.Name, "compositionName", comp.Name, "compositionGeneration", comp.Generation)
+		}
 		return ctrl.Result{}, nil
 	}
 
@@ -73,21 +79,19 @@ func (c *Controller) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Resu
 		return ctrl.Result{}, nil // already in sync
 	}
 
-	// Create a job to generate the resouces!
-	err = c.client.Create(ctx, job)
+	// Create a pod to generate the resouces!
+	err = c.client.Create(ctx, pod)
 	if err != nil {
-		return ctrl.Result{}, client.IgnoreAlreadyExists(fmt.Errorf("creating generation job: %w", err))
+		return ctrl.Result{}, client.IgnoreAlreadyExists(fmt.Errorf("creating generation pod: %w", err))
 	}
-	c.logger.Info("created job to generate resources for composition", "jobName", job.Name, "compositionName", comp.Name, "compositionGeneration", comp.Generation)
+	c.logger.Info("created pod to generate resources for composition", "podName", pod.Name, "compositionName", comp.Name, "compositionGeneration", comp.Generation)
 
 	return ctrl.Result{}, nil
 }
 
-func (c *Controller) newJob(comp *apiv1.Composition, gen *apiv1.Generator) *batchv1.Job {
+func (c *Controller) newPod(comp *apiv1.Composition, gen *apiv1.Generator) *corev1.Pod {
 	var (
-		parallelism       = int32(1)
 		timeout           = int64(c.config.JobTimeout.Seconds())
-		ttl               = int32(c.config.JobTTL.Seconds())
 		wrapperVolumeName = "wrapper"
 	)
 
@@ -95,65 +99,77 @@ func (c *Controller) newJob(comp *apiv1.Composition, gen *apiv1.Generator) *batc
 	fmt.Fprintf(hash, "%s-%d", comp.Name, comp.Generation)
 	hashStr := hex.EncodeToString(hash.Sum(nil))[:7]
 
-	job := &batchv1.Job{}
-	job.Name = "generate-" + hashStr
-	job.Namespace = comp.Namespace
-	if err := controllerutil.SetControllerReference(comp, job, c.client.Scheme()); err != nil {
+	pod := &corev1.Pod{}
+	pod.Name = "generate-" + hashStr
+	pod.Namespace = comp.Namespace
+	if err := controllerutil.SetControllerReference(comp, pod, c.client.Scheme()); err != nil {
 		panic(fmt.Sprintf("unable to set owner reference: %s", err))
 	}
-	job.Spec.Parallelism = &parallelism
-	job.Spec.TTLSecondsAfterFinished = &ttl
-	job.Spec.Template = corev1.PodTemplateSpec{
-		Spec: corev1.PodSpec{
-			RestartPolicy:         corev1.RestartPolicyNever,
-			ActiveDeadlineSeconds: &timeout,
-			InitContainers: []corev1.Container{{
-				Name:  "setup",
-				Image: c.config.WrapperImage,
-				Command: []string{
-					"/eno-wrapper", "--install=/wrapper/eno-wrapper",
-				},
-				VolumeMounts: []corev1.VolumeMount{{
-					Name:      wrapperVolumeName,
-					MountPath: "/wrapper",
-				}},
+	pod.Spec = corev1.PodSpec{
+		RestartPolicy:         corev1.RestartPolicyOnFailure,
+		ActiveDeadlineSeconds: &timeout,
+		InitContainers: []corev1.Container{{
+			Name:  "setup",
+			Image: c.config.WrapperImage,
+			Command: []string{
+				"/eno-wrapper", "--install=/wrapper/eno-wrapper",
+			},
+			VolumeMounts: []corev1.VolumeMount{{
+				Name:      wrapperVolumeName,
+				MountPath: "/wrapper",
 			}},
-			Containers: []corev1.Container{{
-				Name:  "generator",
-				Image: gen.Spec.Image,
-				Command: []string{
-					"/wrapper/eno-wrapper", "--generate",
-				},
-				VolumeMounts: []corev1.VolumeMount{{
-					Name:      wrapperVolumeName,
-					MountPath: "/wrapper",
-					ReadOnly:  true,
-				}},
-				Env: []corev1.EnvVar{
-					{
-						Name:  "COMPOSITION_NAME",
-						Value: comp.Name,
-					},
-					{
-						Name:  "COMPOSITION_NAMESPACE",
-						Value: comp.Namespace,
-					},
-					{
-						Name:  "COMPOSITION_GENERATION",
-						Value: strconv.FormatInt(comp.Generation, 10),
-					},
-				},
+		}},
+		Containers: []corev1.Container{{
+			Name:  "generator",
+			Image: gen.Spec.Image,
+			Command: []string{
+				"/wrapper/eno-wrapper", "--generate",
+			},
+			VolumeMounts: []corev1.VolumeMount{{
+				Name:      wrapperVolumeName,
+				MountPath: "/wrapper",
+				ReadOnly:  true,
 			}},
-			Volumes: []corev1.Volume{{
-				Name:         wrapperVolumeName,
-				VolumeSource: corev1.VolumeSource{EmptyDir: &corev1.EmptyDirVolumeSource{}},
-			}},
-		},
+			Env: []corev1.EnvVar{
+				{
+					Name:  "COMPOSITION_NAME",
+					Value: comp.Name,
+				},
+				{
+					Name:  "COMPOSITION_NAMESPACE",
+					Value: comp.Namespace,
+				},
+				{
+					Name:  "COMPOSITION_GENERATION",
+					Value: strconv.FormatInt(comp.Generation, 10),
+				},
+			},
+		}},
+		Volumes: []corev1.Volume{{
+			Name:         wrapperVolumeName,
+			VolumeSource: corev1.VolumeSource{EmptyDir: &corev1.EmptyDirVolumeSource{}},
+		}},
 	}
 
 	if c.config.JobSA != "" {
-		job.Spec.Template.Spec.ServiceAccountName = c.config.JobSA
+		pod.Spec.ServiceAccountName = c.config.JobSA
 	}
 
-	return job
+	return pod
+}
+
+func shouldDeletePod(pod *corev1.Pod) bool {
+	for _, cont := range pod.Status.ContainerStatuses {
+		// Recreate the pod on another node eventually in case it was scheduled to a broken one
+		if cont.RestartCount > 5 {
+			return true
+		}
+
+		// TODO: Handle pull failures also?
+
+		if cont.State.Terminated == nil || cont.State.Terminated.ExitCode != 0 {
+			return false // has not completed yet
+		}
+	}
+	return len(pod.Status.ContainerStatuses) > 0
 }
