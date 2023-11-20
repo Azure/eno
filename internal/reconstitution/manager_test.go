e2943e01053da634ddac046bf0537b27721a6846
Jordan Olshevski <jordan.olshevski@microsoft.com>
2023-11-20 11:41:38 -0600

    Add unit test

---
+++ internal/reconstitution/manager_test.go
@@ -0,0 +1,73 @@
+package reconstitution
+
+import (
+	"context"
+	"sync/atomic"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
+
+	apiv1 "github.com/Azure/eno/api/v1"
+	"github.com/Azure/eno/internal/testutil"
+)
+
+func TestManagerBasics(t *testing.T) {
+	ctx := testutil.NewContext(t)
+	mgr := testutil.NewManager(t)
+	client := mgr.GetClient()
+
+	rm, err := New(mgr.Manager, time.Millisecond)
+	require.NoError(t, err)
+
+	tr := &testReconciler{mgr: rm}
+	rm.Add(tr)
+	mgr.Start(t)
+
+	// Create one composition that has one synthesis of a single resource
+	comp := &apiv1.Composition{}
+	comp.Name = "test-composition"
+	comp.Namespace = "default"
+	require.NoError(t, client.Create(ctx, comp))
+
+	one := int64(1)
+	comp.Status.CurrentState = &apiv1.Synthesis{
+		ObservedCompositionGeneration: comp.Generation,
+		ResourceSliceCount:            &one,
+	}
+	require.NoError(t, client.Status().Update(ctx, comp))
+
+	slice := &apiv1.ResourceSlice{}
+	slice.Name = "test-slice"
+	slice.Namespace = "default"
+	slice.Spec.CompositionGeneration = comp.Generation
+	slice.Spec.Resources = []apiv1.Manifest{{
+		Manifest: `{"kind":"baz","apiVersion":"any","metadata":{"name":"foo","namespace":"bar"}}`,
+	}}
+	require.NoError(t, controllerutil.SetControllerReference(comp, slice, mgr.GetScheme()))
+	require.NoError(t, client.Create(ctx, slice))
+
+	// The resource is eventually reconciled
+	testutil.Eventually(t, func() bool {
+		res := tr.lastResource.Load()
+		return res != nil && res.Ref.Name == "foo"
+	})
+}
+
+type testReconciler struct {
+	mgr          Manager
+	lastResource atomic.Pointer[Resource]
+}
+
+func (t *testReconciler) Name() string { return "testReconciler" }
+
+func (t *testReconciler) Reconcile(ctx context.Context, req *Request) (ctrl.Result, error) {
+	resource, exists := t.mgr.GetClient().Get(ctx, &req.ResourceRef, 1)
+	if !exists {
+		panic("resource should exist in cache")
+	}
+	t.lastResource.Store(resource)
+	return ctrl.Result{}, nil
+}
