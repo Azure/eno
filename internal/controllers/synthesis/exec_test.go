1676f90f8069c818e67a5c839ffdf95d87ecf756
Jordan Olshevski <joolshev@microsoft.com>
2023-12-29 18:38:50 +0000

    Create tombstones

--- internal/controllers/synthesis/exec_test.go
+++ internal/controllers/synthesis/exec_test.go
@@ -18,9 +18,99 @@ func TestBuildResourceSlicesOverflow(t *testing.T) {
 		outputs = append(outputs, &unstructured.Unstructured{})
 	}
 
-	slices, err := buildResourceSlices(&apiv1.Composition{}, outputs, 20)
+	slices, err := buildResourceSlices(&apiv1.Composition{}, []*apiv1.ResourceSlice{}, outputs, 20)
 	require.NoError(t, err)
 	assert.Len(t, slices, 4)
+}
+
+func TestBuildResourceSlicesTombstonesBasics(t *testing.T) {
+	outputs := []*unstructured.Unstructured{{
+		Object: map[string]interface{}{
+			"kind":       "Test",
+			"apiVersion": "mygroup/v1",
+			"metadata": map[string]interface{}{
+				"name":      "test-resource",
+				"namespace": "test-ns",
+			},
+		},
+	}}
+
+	slices, err := buildResourceSlices(&apiv1.Composition{}, []*apiv1.ResourceSlice{}, outputs, 100000)
+	require.NoError(t, err)
+	require.Len(t, slices, 1)
+	require.Len(t, slices[0].Spec.Resources, 1)
+	assert.False(t, slices[0].Spec.Resources[0].Deleted)
+
+	// Remove the resource - initial tombstone record is created
+	slices, err = buildResourceSlices(&apiv1.Composition{}, slices, []*unstructured.Unstructured{}, 100000)
+	require.NoError(t, err)
+	require.Len(t, slices, 1)
+	require.Len(t, slices[0].Spec.Resources, 1)
+	assert.True(t, slices[0].Spec.Resources[0].Deleted)
+
+	// The actual resource hasn't been reconciled (deleted) yet, so the tombstone will persist in new states
+	slices, err = buildResourceSlices(&apiv1.Composition{}, slices, []*unstructured.Unstructured{}, 100000)
+	require.NoError(t, err)
+	require.Len(t, slices, 1)
+	require.Len(t, slices[0].Spec.Resources, 1)
+	assert.True(t, slices[0].Spec.Resources[0].Deleted)
+
+	// The tombstone is removed once it has been reconciled
+	slices[0].Status.Resources = []apiv1.ResourceState{{Reconciled: true}}
+	slices, err = buildResourceSlices(&apiv1.Composition{}, slices, []*unstructured.Unstructured{}, 100000)
+	require.NoError(t, err)
+	require.Len(t, slices, 0)
+}
+
+func TestBuildResourceSlicesTombstonesVersionSemantics(t *testing.T) {
+	outputs := []*unstructured.Unstructured{{
+		Object: map[string]interface{}{
+			"kind":       "Test",
+			"apiVersion": "mygroup/v1",
+			"metadata": map[string]interface{}{
+				"name":      "test-resource",
+				"namespace": "test-ns",
+			},
+		},
+	}}
+	slices, err := buildResourceSlices(&apiv1.Composition{}, []*apiv1.ResourceSlice{}, outputs, 100000)
+	require.NoError(t, err)
+	require.Len(t, slices, 1)
+	require.Len(t, slices[0].Spec.Resources, 1)
+	assert.False(t, slices[0].Spec.Resources[0].Deleted)
+
+	// Upgrade to v2 - tombstone should not be created
+	outputs = []*unstructured.Unstructured{{
+		Object: map[string]interface{}{
+			"kind":       "Test",
+			"apiVersion": "mygroup/v2",
+			"metadata": map[string]interface{}{
+				"name":      "test-resource",
+				"namespace": "test-ns",
+			},
+		},
+	}}
+	slices, err = buildResourceSlices(&apiv1.Composition{}, slices, outputs, 100000)
+	require.NoError(t, err)
+	require.Len(t, slices, 1)
+	require.Len(t, slices[0].Spec.Resources, 1)
+	assert.False(t, slices[0].Spec.Resources[0].Deleted)
+
+	// Change group name - tombstone should be created
+	outputs = []*unstructured.Unstructured{{
+		Object: map[string]interface{}{
+			"kind":       "Test",
+			"apiVersion": "mygroup2/v2",
+			"metadata": map[string]interface{}{
+				"name":      "test-resource",
+				"namespace": "test-ns",
+			},
+		},
+	}}
+	slices, err = buildResourceSlices(&apiv1.Composition{}, slices, outputs, 100000)
+	require.NoError(t, err)
+	require.Len(t, slices, 1)
+	require.Len(t, slices[0].Spec.Resources, 2)
 }
 
 func TestBuildInputsJson(t *testing.T) {
