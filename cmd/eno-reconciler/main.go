e44412389c3c8ba5792984b0cf3d270792bb00e0
Mariano Uvalle <juvallegarza@microsoft.com>
2024-01-18 00:41:40 +0000

    Overridable remote qps.

--- cmd/eno-reconciler/main.go
+++ cmd/eno-reconciler/main.go
@@ -34,6 +34,7 @@ func run() error {
 		discoveryMaxRPS        float32
 		debugLogging           bool
 		remoteKubeconfigFile   string
+		remoteQPS              float64
 
 		mgrOpts = &manager.Options{
 			Rest: ctrl.GetConfigOrDie(),
@@ -42,7 +43,8 @@ func run() error {
 	flag.BoolVar(&rediscoverWhenNotFound, "rediscover-when-not-found", true, "Invalidate discovery cache when any type is not found in the openapi spec. Set this to false on <= k8s 1.14")
 	flag.DurationVar(&writeBatchInterval, "write-batch-interval", time.Second*5, "The max throughput of composition status updates")
 	flag.BoolVar(&debugLogging, "debug", true, "Enable debug logging")
-	flag.StringVar(&remoteKubeconfigFile, "remote-kubeconfig", "", "Path to the kubeconfig of the cluster where the resources will be reconciled. The config from the environment is used if this is not provided")
+	flag.StringVar(&remoteKubeconfigFile, "remote-kubeconfig", "", "Path to the kubeconfig of the apiserver where the resources will be reconciled. The config from the environment is used if this is not provided")
+	flag.Float64Var(&remoteQPS, "remote-qps", 0, "Max requests per second to the remote apiserver")
 	mgrOpts.Bind(flag.CommandLine)
 	flag.Parse()
 
@@ -70,6 +72,9 @@ func run() error {
 	if remoteKubeconfigFile != "" {
 		if remoteConfig, err = k8s.GetRESTConfig(remoteKubeconfigFile); err != nil {
 			return err
+		}
+		if remoteQPS != 0 {
+			remoteConfig.QPS = float32(remoteQPS)
 		}
 	}
 	err = reconciliation.New(recmgr, remoteConfig, discoveryMaxRPS, rediscoverWhenNotFound)
