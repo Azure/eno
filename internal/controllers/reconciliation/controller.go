Azure/eno

commit d1ef4ad6271c9d0b57d32ad2bc54038d3b96ede7

More perf optimizations (#41)
Avoids some copies and adds logic to the reconciler that avoids parsing
the current state of the entire resource. In the end we can do ~200 RPS
of no-op reconciling at 71m/80Mi down from around 2x that.

---------

Co-authored-by: Jordan Olshevski <joolshev@microsoft.com>

internal/controllers/reconciliation/controller.go
@@ -100,14 +100,14 @@ func (c *Controller) Reconcile(ctx context.Context, req *reconstitution.Request)
 	}
 
 	// Fetch the current resource
-	current := &unstructured.Unstructured{}
-	current.SetName(req.Resource.Name)
-	current.SetNamespace(req.Resource.Namespace)
-	current.SetKind(req.Resource.Kind)
-	current.SetAPIVersion(apiVersion)
-	err = c.upstreamClient.Get(ctx, client.ObjectKeyFromObject(current), current)
+	current, err := c.getCurrent(ctx, resource, apiVersion)
 	if client.IgnoreNotFound(err) != nil {
 		return ctrl.Result{}, fmt.Errorf("getting current state: %w", err)
+	}
+	if current == nil {
+		// This means the resource hasn't changed since last reconciliation
+		// We don't log here since this is the hottest path in Eno
+		return c.continueLoop(resource)
 	}
 
 	// Do the reconciliation
@@ -127,6 +127,10 @@ func (c *Controller) Reconcile(ctx context.Context, req *reconstitution.Request)
 		return
 	})
 
+	return c.continueLoop(resource)
+}
+
+func (c *Controller) continueLoop(resource *reconstitution.Resource) (ctrl.Result, error) {
 	if resource != nil && resource.Manifest.ReconcileInterval != nil {
 		return ctrl.Result{RequeueAfter: wait.Jitter(resource.Manifest.ReconcileInterval.Duration, 0.1)}, nil
 	}
@@ -214,6 +218,35 @@ func (c *Controller) buildPatch(ctx context.Context, prev, resource *reconstitut
 		return nil, "", reconcile.TerminalError(err)
 	}
 	return patch, types.StrategicMergePatchType, err
+}
+
+func (c *Controller) getCurrent(ctx context.Context, resource *reconstitution.Resource, apiVersion string) (*unstructured.Unstructured, error) {
+	if resource.HasBeenSeen() {
+		meta := &metav1.PartialObjectMetadata{}
+		meta.Name = resource.Ref.Name
+		meta.Namespace = resource.Ref.Namespace
+		meta.Kind = resource.Ref.Kind
+		meta.APIVersion = apiVersion
+		err := c.upstreamClient.Get(ctx, client.ObjectKeyFromObject(meta), meta)
+		if err != nil {
+			return nil, err
+		}
+		if resource.MatchesLastSeen(meta.ResourceVersion) {
+			return nil, nil // resource hasn't changed - no need to reconcile
+		}
+	}
+
+	current := &unstructured.Unstructured{}
+	current.SetName(resource.Ref.Name)
+	current.SetNamespace(resource.Ref.Namespace)
+	current.SetKind(resource.Ref.Kind)
+	current.SetAPIVersion(apiVersion)
+	err := c.upstreamClient.Get(ctx, client.ObjectKeyFromObject(current), current)
+	if rv := current.GetResourceVersion(); rv != "" {
+		resource.ObserveVersion(rv)
+	}
+
+	return current, err
 }
 
 func mungePatch(patch []byte, rv string) ([]byte, error) {

internal/controllers/reconciliation/integration_test.go
@@ -561,6 +561,7 @@ func TestCompositionDeletionOrdering(t *testing.T) {
 	// Everything should eventually be cleaned up
 	// This implicitly covers ordering, since it's impossible to delete a resource without its composition
 	testutil.Eventually(t, func() bool {
+		// TODO: Flakes
 		return errors.IsNotFound(downstream.Get(ctx, client.ObjectKeyFromObject(obj), obj)) && errors.IsNotFound(upstream.Get(ctx, client.ObjectKeyFromObject(comp), comp))
 	})
 }

examples/loadtest.yaml
@@ -8,13 +8,13 @@ spec:
   - /bin/bash
   - -c
   - |
-      n=23
+      n=3000
       echo -n '['
       for i in $(seq $n); do
         echo -n "{
           \"apiVersion\": \"v1\",
           \"kind\": \"ConfigMap\",
-          \"metadata\": { \"name\": \"test-cm-${i}\", \"namespace\": \"default\", \"annotations\": { \"eno.azure.io/reconcile-interval\": \"10s\" } },
+          \"metadata\": { \"name\": \"test-cm-${i}\", \"namespace\": \"default\", \"annotations\": { \"eno.azure.io/reconcile-interval\": \"30s\" } },
           \"data\": { \"foo\": \"bar\" }
         }"
         if [[ $i == $n ]]; then

internal/reconstitution/api.go
@@ -2,6 +2,7 @@ package reconstitution
 
 import (
 	"context"
+	"sync"
 
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/types"
@@ -32,8 +33,9 @@ type ManifestRef struct {
 
 // Resource is the controller's internal representation of a single resource out of a ResourceSlice.
 type Resource struct {
-	Ref *ResourceRef
+	*lastSeenMeta
 
+	Ref      *ResourceRef
 	Manifest *apiv1.Manifest
 	Object   *unstructured.Unstructured
 }
@@ -63,4 +65,27 @@ type Request struct {
 	Resource    ResourceRef
 	Manifest    ManifestRef
 	Composition CompositionRef
+}
+
+type lastSeenMeta struct {
+	lock            sync.Mutex
+	resourceVersion string
+}
+
+func (l *lastSeenMeta) ObserveVersion(rv string) {
+	l.lock.Lock()
+	defer l.lock.Unlock()
+	l.resourceVersion = rv
+}
+
+func (l *lastSeenMeta) HasBeenSeen() bool {
+	l.lock.Lock()
+	defer l.lock.Unlock()
+	return l.resourceVersion != ""
+}
+
+func (l *lastSeenMeta) MatchesLastSeen(rv string) bool {
+	l.lock.Lock()
+	defer l.lock.Unlock()
+	return l.resourceVersion == rv
 }

internal/reconstitution/cache.go
@@ -47,9 +47,10 @@ func (c *cache) Get(ctx context.Context, comp *CompositionRef, ref *ResourceRef)
 	// Copy the resource so it's safe for callers to mutate
 	refDeref := *ref
 	return &Resource{
-		Ref:      &refDeref,
-		Manifest: res.Manifest.DeepCopy(),
-		Object:   res.Object.DeepCopy(),
+		lastSeenMeta: res.lastSeenMeta,
+		Ref:          &refDeref,
+		Manifest:     res.Manifest.DeepCopy(),
+		Object:       res.Object,
 	}, ok
 }
 
@@ -139,6 +140,7 @@ func (c *cache) buildResource(ctx context.Context, comp *apiv1.Composition, slic
 	}
 
 	res := &Resource{
+		lastSeenMeta: &lastSeenMeta{},
 		Ref: &ResourceRef{
 			Namespace: parsed.GetNamespace(),
 			Name:      parsed.GetName(),

commit f51af3e5dad2cffdc3abfd94093512a9e0ab16f6

Fix status update flake (#45)
The most common test flake is due to a race caused by #41.

Essentially there's a race where if a resource has already hit the cache
it won't ever be marked as deleted.

---------

Co-authored-by: Jordan Olshevski <joolshev@microsoft.com>

dev/deploy.yaml
@@ -41,6 +41,7 @@ spec:
         image: $REGISTRY/eno-reconciler:$TAG
         args:
         - --qps=1000
+        - --remote-qps=1000
         env:
           - name: PPROF_ADDR
             value: ":8888"

internal/controllers/reconciliation/controller.go
@@ -104,15 +104,13 @@ func (c *Controller) Reconcile(ctx context.Context, req *reconstitution.Request)
 	if client.IgnoreNotFound(err) != nil {
 		return ctrl.Result{}, fmt.Errorf("getting current state: %w", err)
 	}
-	if current == nil {
-		// This means the resource hasn't changed since last reconciliation
-		// We don't log here since this is the hottest path in Eno
-		return c.continueLoop(resource)
-	}
 
-	// Do the reconciliation
-	if err := c.reconcileResource(ctx, prev, resource, current); err != nil {
-		return ctrl.Result{}, err
+	// Nil current struct means the resource version hasn't changed since it was last observed
+	// Skip without logging since this is a very hot path
+	if current != nil {
+		if err := c.reconcileResource(ctx, prev, resource, current); err != nil {
+			return ctrl.Result{}, err
+		}
 	}
 
 	c.resourceClient.PatchStatusAsync(ctx, &req.Manifest, func(rs *apiv1.ResourceState) (modified bool) {
@@ -127,13 +125,10 @@ func (c *Controller) Reconcile(ctx context.Context, req *reconstitution.Request)
 		return
 	})
 
-	return c.continueLoop(resource)
-}
-
-func (c *Controller) continueLoop(resource *reconstitution.Resource) (ctrl.Result, error) {
 	if resource != nil && resource.Manifest.ReconcileInterval != nil {
 		return ctrl.Result{RequeueAfter: wait.Jitter(resource.Manifest.ReconcileInterval.Duration, 0.1)}, nil
 	}
+
 	return ctrl.Result{}, nil
 }
 

internal/manager/manager.go
@@ -74,6 +74,9 @@ func newMgr(logger logr.Logger, opts *Options, enableIndexing bool) (ctrl.Manage
 		Metrics: server.Options{
 			BindAddress: opts.MetricsAddr,
 		},
+		BaseContext: func() context.Context {
+			return logr.NewContext(context.Background(), logger)
+		},
 	}
 
 	labelSelector, err := opts.getDefaultLabelSelector()

internal/reconstitution/manager.go
@@ -17,7 +17,7 @@ func New(mgr ctrl.Manager, writeBatchInterval time.Duration) (*Manager, error) {
 		Manager: mgr,
 	}
 	m.writeBuffer = newWriteBuffer(mgr.GetClient(), writeBatchInterval, 2)
-	mgr.Add(m.writeBuffer) // TODO: No logger is in the context here
+	mgr.Add(m.writeBuffer)
 
 	var err error
 	m.reconstituter, err = newReconstituter(mgr)
@@ -37,7 +37,7 @@ type Manager struct {
 func (m *Manager) GetClient() Client { return m }
 
 func (m *Manager) Add(rec Reconciler) error {
-	rateLimiter := workqueue.DefaultControllerRateLimiter()
+	rateLimiter := workqueue.DefaultItemBasedRateLimiter()
 	queue := workqueue.NewRateLimitingQueueWithConfig(rateLimiter, workqueue.RateLimitingQueueConfig{
 		Name: rec.Name(),
 	})

internal/reconstitution/writebuffer.go
@@ -54,7 +54,7 @@ func (w *writeBuffer) PatchStatusAsync(ctx context.Context, ref *ManifestRef, pa
 		SlicedResource: ref,
 		PatchFn:        patchFn,
 	})
-	w.queue.AddRateLimited(key)
+	w.queue.Add(key)
 }
 
 func (w *writeBuffer) Start(ctx context.Context) error {
@@ -84,11 +84,13 @@ func (w *writeBuffer) processQueueItem(ctx context.Context) bool {
 	w.mut.Unlock()
 
 	if len(updates) == 0 {
-		logger.V(0).Info("dropping queue item because no updates were found for this slice (this is suspicious)")
+		w.queue.Forget(item)
+		return true // nothing to do
 	}
 
 	if w.updateSlice(ctx, sliceNSN, updates) {
 		w.queue.Forget(item)
+		w.queue.AddRateLimited(item)
 		return true
 	}
 
@@ -108,6 +110,7 @@ func (w *writeBuffer) updateSlice(ctx context.Context, sliceNSN types.Namespaced
 	slice := &apiv1.ResourceSlice{}
 	err := w.client.Get(ctx, sliceNSN, slice)
 	if errors.IsNotFound(err) {
+		// TODO: I think this should cause the work queue to Forget this item?
 		logger.V(0).Info("slice has been deleted, skipping status update")
 		return true
 	}

internal/controllers/reconciliation/integration_test.go
@@ -561,7 +561,6 @@ func TestCompositionDeletionOrdering(t *testing.T) {
 	// Everything should eventually be cleaned up
 	// This implicitly covers ordering, since it's impossible to delete a resource without its composition
 	testutil.Eventually(t, func() bool {
-		// TODO: Flakes
 		return errors.IsNotFound(downstream.Get(ctx, client.ObjectKeyFromObject(obj), obj)) && errors.IsNotFound(upstream.Get(ctx, client.ObjectKeyFromObject(comp), comp))
 	})
 }

internal/reconstitution/writebuffer_test.go
@@ -44,6 +44,7 @@ func TestWriteBufferBasics(t *testing.T) {
 
 	// All state has been flushed
 	assert.Len(t, w.state, 0)
+	w.processQueueItem(ctx)
 	assert.Equal(t, 0, w.queue.Len())
 }
 
@@ -122,6 +123,7 @@ func TestWriteBufferMissingSlice(t *testing.T) {
 
 	// Slice 404 drops the event and does not retry.
 	// Prevents a deadlock of this queue item.
+	w.processQueueItem(ctx)
 	w.processQueueItem(ctx)
 	assert.Equal(t, 0, w.queue.Len())
 }