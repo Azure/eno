bc8dd531cf46711140b472f7606ca489f5644510
Jordan Olshevski <joolshev@microsoft.com>
2024-02-26 19:05:50 +0000

    More logging for flaky test

--- internal/controllers/reconciliation/integration_test.go
+++ internal/controllers/reconciliation/integration_test.go
@@ -566,6 +566,9 @@ func TestCompositionDeletionOrdering(t *testing.T) {
 	// Everything should eventually be cleaned up
 	// This implicitly covers ordering, since it's impossible to delete a resource without its composition
 	testutil.Eventually(t, func() bool {
-		return errors.IsNotFound(downstream.Get(ctx, client.ObjectKeyFromObject(obj), obj)) && errors.IsNotFound(upstream.Get(ctx, client.ObjectKeyFromObject(comp), comp))
+		resourceGone := errors.IsNotFound(downstream.Get(ctx, client.ObjectKeyFromObject(obj), obj))
+		compGone := errors.IsNotFound(upstream.Get(ctx, client.ObjectKeyFromObject(comp), comp))
+		t.Logf("resourceGone=%t compGone=%t", resourceGone, compGone)
+		return resourceGone && compGone
 	})
 }
