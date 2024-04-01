6ce33ada8b0d962af6c1020d56f257870c6fc447
Jordan Olshevski <joolshev@microsoft.com>
2024-04-01 18:31:27 +0000

    Finish basic implementation

--- internal/controllers/replication/symphony.go
+++ internal/controllers/replication/symphony.go
@@ -18,6 +18,8 @@ type symphonyController struct {
 	client client.Client
 }
 
+// TODO: Avoid conflicts
+
 func NewCompositionSetController(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&apiv1.Symphony{}).
@@ -37,50 +39,95 @@ func (c *symphonyController) Reconcile(ctx context.Context, req ctrl.Request) (c
 		return ctrl.Result{}, client.IgnoreNotFound(err)
 	}
 	logger = logger.WithValues("compositionSetName", symph.Name, "compositionSetNamespace", symph.Namespace)
+	ctx = logr.NewContext(ctx, logger)
+
+	existing := &apiv1.CompositionList{}
+	err = c.client.List(ctx, existing, client.MatchingFields{
+		manager.IdxCompositionsBySymphony: symph.Name,
+	})
+	if err != nil {
+		return ctrl.Result{}, fmt.Errorf("listing existing compositions: %w", err)
+	}
+
+	// Hold a finalizer
+	if controllerutil.AddFinalizer(symph, "eno.azure.io/cleanup") {
+		err := c.client.Update(ctx, symph)
+		if err != nil {
+			return ctrl.Result{}, fmt.Errorf("adding finalizer: %w", err)
+		}
+		return ctrl.Result{}, nil
+	}
+
+	existingBySynthName, err := c.reconcileReverse(ctx, symph, existing)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+	if symph.DeletionTimestamp == nil {
+		err = c.reconcileForward(ctx, symph, existingBySynthName)
+		if err != nil {
+			return ctrl.Result{}, err
+		}
+	}
+
+	// Release the finalizer when no compositions exists
+	if symph.DeletionTimestamp != nil {
+		if len(existing.Items) > 0 {
+			return ctrl.Result{}, nil // wait for composition deletion
+		}
+		if controllerutil.RemoveFinalizer(symph, "eno.azure.io/cleanup") {
+			err := c.client.Update(ctx, symph)
+			if err != nil {
+				return ctrl.Result{}, fmt.Errorf("removing finalizer: %w", err)
+			}
+		}
+	}
+
+	return ctrl.Result{}, nil
+}
+
+func (c *symphonyController) reconcileReverse(ctx context.Context, symph *apiv1.Symphony, comps *apiv1.CompositionList) (map[string][]*apiv1.Composition, error) {
+	logger := logr.FromContextOrDiscard(ctx)
+
 	expectedSynths := map[string]struct{}{}
 	for _, syn := range symph.Spec.Synthesizers {
 		expectedSynths[syn.Name] = struct{}{}
 	}
 
-	existing := &apiv1.CompositionList{}
-	err = c.client.List(ctx, existing) // TODO: INdex
-	if err != nil {
-		return ctrl.Result{}, fmt.Errorf("listing existing compositions: %w", err)
-	}
-
 	existingBySynthName := map[string][]*apiv1.Composition{}
-	for _, comp := range existing.Items {
+	for _, comp := range comps.Items {
 		comp := comp
 		existingBySynthName[comp.Spec.Synthesizer.Name] = append(existingBySynthName[comp.Spec.Synthesizer.Name], &comp)
 
-		if _, ok := expectedSynths[comp.Spec.Synthesizer.Name]; ok || comp.DeletionTimestamp == nil {
+		if _, ok := expectedSynths[comp.Spec.Synthesizer.Name]; (ok || comp.DeletionTimestamp == nil) && symph.DeletionTimestamp == nil {
 			continue // should still exist, or already deleting
 		}
-
-		// Synth no longer exists in set - remove the composition
 		err := c.client.Delete(ctx, &comp)
 		if err != nil {
-			return ctrl.Result{}, fmt.Errorf("cleaning up composition: %w", err)
+			return nil, fmt.Errorf("cleaning up composition: %w", err)
 		}
 		logger.V(0).Info("deleted composition because its synthesizer was removed from the set", "compositionName", comp.Name, "compositionNamespace", comp.Namespace)
 	}
+
 	for _, comps := range existingBySynthName {
 		if len(comps) < 2 {
 			continue
 		}
 
-		// Oldest first
-		// TODO: Test
 		sort.Slice(comps, func(i, j int) bool { return comps[i].CreationTimestamp.Before(&comps[j].CreationTimestamp) })
 
 		err := c.client.Delete(ctx, comps[0])
 		if err != nil {
-			return ctrl.Result{}, fmt.Errorf("deleting duplicate composition: %w", err)
+			return nil, fmt.Errorf("deleting duplicate composition: %w", err)
 		}
 		logger.V(0).Info("deleted composition because it's a duplicate", "compositionName", comps[0].Name, "compositionNamespace", comps[0].Namespace)
 	}
 
-	// TODO: Set finalizer
+	return existingBySynthName, nil
+}
+
+func (c *symphonyController) reconcileForward(ctx context.Context, symph *apiv1.Symphony, existingBySynthName map[string][]*apiv1.Composition) error {
+	logger := logr.FromContextOrDiscard(ctx)
+
 	for _, synRef := range symph.Spec.Synthesizers {
 		synRef := synRef
 		comp := &apiv1.Composition{}
@@ -88,31 +135,32 @@ func (c *symphonyController) Reconcile(ctx context.Context, req ctrl.Request) (c
 		comp.GenerateName = synRef.Name + "-"
 		comp.Spec.Bindings = symph.Spec.Bindings
 		comp.Spec.Synthesizer = synRef
-		err = controllerutil.SetControllerReference(symph, comp, c.client.Scheme())
+		err := controllerutil.SetControllerReference(symph, comp, c.client.Scheme())
 		if err != nil {
-			return ctrl.Result{}, fmt.Errorf("setting composition's controller: %w", err)
+			return fmt.Errorf("setting composition's controller: %w", err)
 		}
 
 		// Diff and update if needed when the composition for this synthesizer already exists
-		if existing, ok := existingBySynthName[synRef.Name]; ok {
-			if equality.Semantic.DeepEqual(comp.Spec, existing[0].Spec) {
+		if existings, ok := existingBySynthName[synRef.Name]; ok {
+			existing := existings[0]
+			if equality.Semantic.DeepEqual(comp.Spec, existing.Spec) {
 				continue // already matches
 			}
 
-			comp.ResourceVersion = existing[0].ResourceVersion
-			err = c.client.Update(ctx, comp)
+			existing.Spec = comp.Spec
+			err = c.client.Update(ctx, existing)
 			if err != nil {
-				return ctrl.Result{}, fmt.Errorf("updating existing composition: %w", err)
+				return fmt.Errorf("updating existing composition: %w", err)
 			}
-			logger.V(0).Info("updated composition because the set's spec changed", "compositionName", existing[0].Name, "compositionNamespace", existing[0].Namespace)
+			logger.V(0).Info("updated composition because the set's spec changed", "compositionName", existing.Name, "compositionNamespace", existing.Namespace)
 		}
 
 		err = c.client.Create(ctx, comp)
 		if err != nil {
-			return ctrl.Result{}, fmt.Errorf("creating composition: %w", err)
+			return fmt.Errorf("creating composition: %w", err)
 		}
 		logger.V(0).Info("created composition for the set", "compositionName", comp.Name, "compositionNamespace", comp.Namespace)
 	}
 
-	return ctrl.Result{}, nil
+	return nil
 }
--- internal/controllers/replication/symphony_test.go
+++ internal/controllers/replication/symphony_test.go
@@ -3,10 +3,16 @@ package replication
 import (
 	"reflect"
 	"testing"
+	"time"
 
 	apiv1 "github.com/Azure/eno/api/v1"
 	"github.com/Azure/eno/internal/testutil"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/util/retry"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
 func TestSymphonyCRUD(t *testing.T) {
@@ -63,4 +69,64 @@ func TestSymphonyCRUD(t *testing.T) {
 		}
 		return true
 	})
+
+	// Update the bindings and prove the new bindings are replicated to the compositions
+	err = retry.RetryOnConflict(testutil.Backoff, func() error {
+		cli.Get(ctx, client.ObjectKeyFromObject(sym), sym)
+		sym.Spec.Bindings = []apiv1.Binding{{Key: "new-binding", Resource: apiv1.ResourceBinding{Name: "foo"}}}
+		return cli.Update(ctx, sym)
+	})
+	require.NoError(t, err)
+
+	testutil.Eventually(t, func() bool {
+		comps := &apiv1.CompositionList{}
+		err := cli.List(ctx, comps)
+		if err != nil && len(comps.Items) < 2 {
+			return false
+		}
+		for _, comp := range comps.Items {
+			if !reflect.DeepEqual(sym.Spec.Bindings, comp.Spec.Bindings) {
+				t.Logf("composition %q has incorrect bindings", comp.Name)
+				return false
+			}
+		}
+		return true
+	})
+
+	// Test deletion
+	require.NoError(t, cli.Delete(ctx, sym))
+	testutil.Eventually(t, func() bool {
+		return errors.IsNotFound(cli.Get(ctx, client.ObjectKeyFromObject(sym), sym))
+	})
+
+	comps := &apiv1.CompositionList{}
+	err = cli.List(ctx, comps)
+	require.NoError(t, err)
+	assert.Len(t, comps.Items, 0)
+}
+
+// TestSymphonyDuplicateCleanup proves that the newest compositions are deleted if multiple exist for a given synthesizer.
+func TestSymphonyDuplicateCleanup(t *testing.T) {
+	ctx := testutil.NewContext(t)
+	cli := testutil.NewClient(t)
+	s := &symphonyController{client: cli}
+
+	sym := &apiv1.Symphony{}
+	sym.Name = "test"
+	sym.Namespace = "default"
+	sym.Spec.Synthesizers = []apiv1.SynthesizerRef{{Name: "foo"}}
+	require.NoError(t, cli.Create(ctx, sym))
+
+	comp := apiv1.Composition{}
+	now := metav1.Now()
+	comp.CreationTimestamp = metav1.NewTime(now.Add(time.Second))
+	comp.Name = "foo"
+
+	comp2 := apiv1.Composition{}
+	comp2.CreationTimestamp = now
+	comp2.Name = "bar"
+
+	comps := &apiv1.CompositionList{Items: []apiv1.Composition{comp, comp2}}
+	_, err := s.reconcileReverse(ctx, sym, comps)
+	require.EqualError(t, err, `deleting duplicate composition: compositions.eno.azure.io "bar" not found`)
 }
