3031e46da4a2ee9278dc605bce8d7a3e0e184db0
Jordan Olshevski <joolshev@microsoft.com>
2024-06-07 03:30:51 +0000

    Refactor

--- internal/execution/executor.go
+++ internal/execution/executor.go
@@ -1,12 +1,8 @@
 package execution
 
 import (
-	"bytes"
 	"context"
-	"encoding/json"
 	"fmt"
-	"os"
-	"os/exec"
 	"strconv"
 	"time"
 
@@ -24,20 +20,6 @@ import (
 
 // maxSliceJsonBytes is the max sum of a resource slice's manifests.
 const maxSliceJsonBytes = 1024 * 512
-
-type Env struct {
-	CompositionName      string
-	CompositionNamespace string
-	SynthesisUUID        string
-}
-
-func LoadEnv() *Env {
-	return &Env{
-		CompositionName:      os.Getenv("COMPOSITION_NAME"),
-		CompositionNamespace: os.Getenv("COMPOSITION_NAMESPACE"),
-		SynthesisUUID:        os.Getenv("SYNTHESIS_UUID"),
-	}
-}
 
 type Executor struct {
 	Reader  client.Reader
@@ -232,47 +214,4 @@ func (e *Executor) updateComposition(ctx context.Context, oldComp *apiv1.Composi
 		logger.V(0).Info("composition status has been updated following successful synthesis")
 		return nil
 	})
-}
-
-type SynthesizerHandle func(context.Context, *apiv1.Synthesizer, *krmv1.ResourceList) (*krmv1.ResourceList, error)
-
-func NewExecHandler() SynthesizerHandle {
-	return func(ctx context.Context, s *apiv1.Synthesizer, rl *krmv1.ResourceList) (*krmv1.ResourceList, error) {
-		stdin := &bytes.Buffer{}
-		stdout := &bytes.Buffer{}
-
-		err := json.NewEncoder(stdin).Encode(rl)
-		if err != nil {
-			return nil, err
-		}
-
-		command := s.Spec.Command
-		if len(command) == 0 {
-			command = []string{"synthesize"}
-		}
-
-		if s.Spec.ExecTimeout != nil {
-			var cancel context.CancelFunc
-			ctx, cancel = context.WithTimeout(ctx, s.Spec.ExecTimeout.Duration)
-			defer cancel()
-		}
-
-		cmd := exec.CommandContext(ctx, command[0], command[1:]...)
-		cmd.Env = []string{} // no env
-		cmd.Stdin = stdin
-		cmd.Stderr = os.Stdout // logger uses stderr, so use stdout to avoid race condition
-		cmd.Stdout = stdout
-		err = cmd.Run()
-		if err != nil {
-			return nil, err
-		}
-
-		output := &krmv1.ResourceList{}
-		err = json.NewDecoder(stdout).Decode(output)
-		if err != nil {
-			return nil, err
-		}
-
-		return output, nil
-	}
 }
--- internal/execution/executor_test.go
+++ internal/execution/executor_test.go
@@ -3,14 +3,12 @@ package execution
 import (
 	"context"
 	"testing"
-	"time"
 
 	apiv1 "github.com/Azure/eno/api/v1"
 	krmv1 "github.com/Azure/eno/pkg/krm/functions/api/v1"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	corev1 "k8s.io/api/core/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
@@ -260,48 +258,4 @@ func TestWithVersionedInput(t *testing.T) {
 	require.NoError(t, err)
 	require.Len(t, comp.Status.CurrentSynthesis.InputRevisions, 1)
 	assert.Equal(t, 123, *comp.Status.CurrentSynthesis.InputRevisions[0].Revision)
-}
-
-func TestExecHandler(t *testing.T) {
-	handle := NewExecHandler()
-
-	syn := &apiv1.Synthesizer{}
-	syn.Spec.Command = []string{"/bin/sh", "-c", "cat /dev/stdin > /dev/stdout"}
-	rl := &krmv1.ResourceList{Items: []*unstructured.Unstructured{{
-		Object: map[string]any{
-			"apiVersion": "v1",
-			"kind":       "ConfigMap",
-			"metadata": map[string]string{
-				"name":      "test",
-				"namespace": "default",
-			},
-			"data": map[string]string{"foo": "bar"},
-		},
-	}}}
-
-	out, err := handle(context.Background(), syn, rl)
-	require.NoError(t, err)
-	require.Len(t, out.Items, 1)
-}
-
-func TestExecHandlerTimeout(t *testing.T) {
-	handle := NewExecHandler()
-
-	syn := &apiv1.Synthesizer{}
-	syn.Spec.Command = []string{"/bin/sh", "-c", "sleep 1"}
-	syn.Spec.ExecTimeout = &metav1.Duration{Duration: time.Millisecond}
-	rl := &krmv1.ResourceList{}
-
-	_, err := handle(context.Background(), syn, rl)
-	require.EqualError(t, err, "signal: killed")
-}
-
-func TestExecHandlerEmpty(t *testing.T) {
-	handle := NewExecHandler()
-
-	syn := &apiv1.Synthesizer{}
-	rl := &krmv1.ResourceList{}
-
-	_, err := handle(context.Background(), syn, rl)
-	require.EqualError(t, err, "exec: \"synthesize\": executable file not found in $PATH")
 }
