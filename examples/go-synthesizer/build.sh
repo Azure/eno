b0dff364a8840498c9ae7ebae83d5ec4b96f30ef
Jordan Olshevski <jveski@users.noreply.github.com>
2024-07-12 08:35:17 -0500

    Improve examples + smoke testing (#155)
    Adds a Go synthesizer example and makes a few improvements to the smoke
    testing tooling:
    
    Now the smoke tests:
    
    - Prove that all examples eventually converge
    - Run the Eno controllers in real pods to test the manifest
    
    So we can just add examples in the future to increase smoke test
    coverage 🚒
    
    ---------
    
    Co-authored-by: Jordan Olshevski <joolshev@microsoft.com>

---
+++ examples/go-synthesizer/build.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -e
+
+if [[ -z "${REGISTRY}" ]]; then
+    echo "REGISTRY must be set" > /dev/stderr
+    exit 1
+fi
+
+TAG="$(date +%s)"
+export IMAGE="$REGISTRY/example-go-synthesizer:$TAG"
+
+docker build --quiet -t ${IMAGE} -f "examples/go-synthesizer/Dockerfile" .
+[[ -z "${SKIP_PUSH}" ]] && docker push ${IMAGE}
+
+kubectl apply -f - <<YAML
+    apiVersion: eno.azure.io/v1
+    kind: Synthesizer
+    metadata:
+      name: go-synth-example-synth
+    spec:
+      image: $IMAGE
+      refs:
+        - key: example-input
+          resource:
+            group: "" # core
+            version: "v1"
+            kind: ConfigMap
+YAML
