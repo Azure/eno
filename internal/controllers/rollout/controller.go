d4551c1dc403a15fd7b4d0898b09b682880df801
Jordan Olshevski <joolshev@microsoft.com>
2024-07-19 16:56:23 +0000

    Fix loop bug

--- internal/controllers/rollout/controller.go
+++ internal/controllers/rollout/controller.go
@@ -71,7 +71,7 @@ func (c *controller) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Resu
 	})
 
 	for _, comp := range comps.Items {
-		logger = logger.WithValues("compositionName", comp.Name, "compositionNamespace", comp.Namespace, "compositionGeneration", comp.Generation)
+		logger := logger.WithValues("compositionName", comp.Name, "compositionNamespace", comp.Namespace, "compositionGeneration", comp.Generation)
 		if comp.Status.PendingResynthesis == nil || comp.Status.CurrentSynthesis == nil {
 			continue
 		}
