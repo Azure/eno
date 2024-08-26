fc3125bc494e07b139327e9516e36afdecf54ffe
Jordan Olshevski <joolshev@microsoft.com>
2024-08-26 21:09:53 +0000

    Increase graceperiod in tests

--- internal/controllers/liveness/namespace_test.go
+++ internal/controllers/liveness/namespace_test.go
@@ -48,7 +48,7 @@ func testMissingNamespace(t *testing.T, orphan client.Object) {
 	mgr := testutil.NewManager(t, testutil.WithCompositionNamespace(ns.Name))
 	cli := mgr.GetClient()
 
-	require.NoError(t, NewNamespaceController(mgr.Manager, time.Millisecond*200))
+	require.NoError(t, NewNamespaceController(mgr.Manager, time.Second))
 	mgr.Start(t)
 
 	require.NoError(t, cli.Create(ctx, ns))
