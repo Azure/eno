6cfa690bf0faf3a1b8a21d265a2a83b7f11f69fa
Jordan Olshevski <jveski@users.noreply.github.com>
2025-01-10 15:43:42 -0600

    Simplify watchdog metrics (#267)
    - Refactor how the metrics are evaluated
    - Add label for synthesizer name
    - Consolidate input-related metrics
    - Remove redundant metrics
    
    Co-authored-by: Jordan Olshevski <joolshev@microsoft.com>

--- internal/controllers/watchdog/controller.go
+++ internal/controllers/watchdog/controller.go
@@ -6,6 +6,7 @@ import (
 
 	apiv1 "github.com/Azure/eno/api/v1"
 	"github.com/Azure/eno/internal/manager"
+	"github.com/prometheus/client_golang/prometheus"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
@@ -35,78 +36,39 @@ func (c *watchdogController) Reconcile(ctx context.Context, req ctrl.Request) (c
 		return ctrl.Result{}, err
 	}
 
-	var inputsMissing int
-	var notInLockstep int
-	var withoutSynthesizers int
-	var pendingInit int
-	var pending int
-	var unready int
-	var terminal int
-	for _, comp := range list.Items {
-		if c.hasNoSynthesizer(&comp, ctx) {
-			withoutSynthesizers++
-		}
-		if c.waitingOnInputs(&comp, ctx) {
-			inputsMissing++
-		}
-		if c.getNotInLockstep(&comp, ctx) {
-			notInLockstep++
-		}
-		if c.pendingInitialReconciliation(&comp) {
-			pendingInit++
-		}
-		if c.pendingReconciliation(&comp) {
-			pending++
-		}
-		if c.pendingReadiness(&comp) {
-			unready++
-		}
-		if c.inTerminalError(&comp) {
-			terminal++
-		}
+	accumulators := []*accumulator{
+		{
+			Predicate: c.waitingOnInputs,
+			Sink:      inputsMissing,
+		},
+		{
+			Predicate: c.pendingReconciliation,
+			Sink:      stuckReconciling,
+		},
+		{
+			Predicate: c.pendingReadiness,
+			Sink:      nonready,
+		},
+		{
+			Predicate: c.inTerminalError,
+			Sink:      terminalErrors,
+		},
 	}
 
-	waitingOnInputs.Set(float64(inputsMissing))
-	inputsNotInLockstep.Set(float64(notInLockstep))
-	compositionsWithoutSynthesizers.Set(float64(withoutSynthesizers))
-	pendingInitialReconciliation.Set(float64(pendingInit))
-	stuckReconciling.Set(float64(pending))
-	pendingReadiness.Set(float64(unready))
-	terminalErrors.Set(float64(terminal))
+	for _, comp := range list.Items {
+		for _, a := range accumulators {
+			a.Visit(&comp)
+		}
+	}
 
 	return ctrl.Result{}, nil
 }
 
-func (c *watchdogController) getSynthesizer(comp *apiv1.Composition, ctx context.Context) (*apiv1.Synthesizer, error) {
+func (c *watchdogController) waitingOnInputs(comp *apiv1.Composition) bool {
 	syn := &apiv1.Synthesizer{}
 	syn.Name = comp.Spec.Synthesizer.Name
-	err := c.client.Get(ctx, client.ObjectKeyFromObject(syn), syn)
-	return syn, err
-}
-
-func (c *watchdogController) hasNoSynthesizer(comp *apiv1.Composition, ctx context.Context) bool {
-	_, err := c.getSynthesizer(comp, ctx)
-	return err != nil
-}
-
-func (c *watchdogController) getInputsExist(comp *apiv1.Composition, ctx context.Context) bool {
-	syn, err := c.getSynthesizer(comp, ctx)
-	return (err != nil) || comp.InputsExist(syn)
-}
-
-func (c *watchdogController) getNotInLockstep(comp *apiv1.Composition, ctx context.Context) bool {
-	syn, err := c.getSynthesizer(comp, ctx)
-	return (err != nil) || comp.InputsOutOfLockstep(syn)
-}
-
-func (c *watchdogController) waitingOnInputs(comp *apiv1.Composition, ctx context.Context) bool {
-	return !c.getInputsExist(comp, ctx) && time.Since(comp.CreationTimestamp.Time) > c.threshold
-}
-
-func (c *watchdogController) pendingInitialReconciliation(comp *apiv1.Composition) bool {
-	return !synthesisHasReconciled(comp.Status.CurrentSynthesis) &&
-		!synthesisHasReconciled(comp.Status.PreviousSynthesis) &&
-		time.Since(comp.CreationTimestamp.Time) > c.threshold
+	err := c.client.Get(context.Background(), client.ObjectKeyFromObject(syn), syn)
+	return err == nil && (!comp.InputsExist(syn) || comp.InputsOutOfLockstep(syn))
 }
 
 func (c *watchdogController) pendingReconciliation(comp *apiv1.Composition) bool {
@@ -118,7 +80,6 @@ func (c *watchdogController) pendingReconciliation(comp *apiv1.Composition) bool
 
 func (c *watchdogController) pendingReadiness(comp *apiv1.Composition) bool {
 	return !synthesisIsReady(comp.Status.CurrentSynthesis) &&
-		!synthesisIsReady(comp.Status.PreviousSynthesis) &&
 		c.timeSinceReconcilePastThreshold(comp)
 }
 
@@ -133,3 +94,21 @@ func (c *watchdogController) timeSinceReconcilePastThreshold(comp *apiv1.Composi
 
 func synthesisHasReconciled(syn *apiv1.Synthesis) bool { return syn != nil && syn.Reconciled != nil }
 func synthesisIsReady(syn *apiv1.Synthesis) bool       { return syn != nil && syn.Ready != nil }
+
+type accumulator struct {
+	Predicate func(*apiv1.Composition) bool
+	Sink      *prometheus.GaugeVec
+	init      bool
+}
+
+func (a *accumulator) Visit(source *apiv1.Composition) {
+	if !a.init {
+		a.Sink.Reset()
+		a.init = true
+	}
+	if a.Predicate(source) {
+		a.Sink.WithLabelValues(source.Spec.Synthesizer.Name).Add(1)
+	} else {
+		a.Sink.WithLabelValues(source.Spec.Synthesizer.Name).Add(0)
+	}
+}
