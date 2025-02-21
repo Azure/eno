f63a3d705ac16364019e320eb17268f148b59adc
Chih-Sheng Huang <pedoe1989@gmail.com>
2025-02-21 10:27:24 -0800

    helmshim: call write() for default output writer only in RenderChart func (#284)
    Call write() for default output writer only in RenderChart func. If the
    client provides an output writer, it should handle write() itself for
    flexibility.

--- pkg/helmshim/helm_test.go
+++ pkg/helmshim/helm_test.go
@@ -16,8 +16,9 @@ func TestRenderChart(t *testing.T) {
 	input := bytes.NewBufferString(`{ "items": [{ "apiVersion": "v1", "kind": "ConfigMap", "metadata": { "name": "test-cm", "annotations": { "eno.azure.io/input-key": "foo" } } }] }`)
 	i, err := function.NewInputReader(input)
 	require.NoError(t, err)
-
 	err = RenderChart(WithInputReader(i), WithOutputWriter(o), WithChartPath("fixtures/basic-chart"))
+	require.NoError(t, err)
+	err = o.Write()
 	require.NoError(t, err)
 	assert.Equal(t, "{\"apiVersion\":\"config.kubernetes.io/v1\",\"kind\":\"ResourceList\",\"items\":[{\"apiVersion\":\"v1\",\"data\":{\"input\":\"{\\\"apiVersion\\\":\\\"v1\\\",\\\"kind\\\":\\\"ConfigMap\\\",\\\"metadata\\\":{\\\"annotations\\\":{\\\"eno.azure.io/input-key\\\":\\\"foo\\\"},\\\"name\\\":\\\"test-cm\\\"}}\",\"inputResourceName\":\"test-cm\",\"some\":\"value\"},\"kind\":\"ConfigMap\",\"metadata\":{\"name\":null}},{\"apiVersion\":\"somegroup.io/v9001\",\"kind\":\"ATypeNotKnownByTheScheme\",\"metadata\":{\"name\":\"foo\"}}]}\n", output.String())
 }
@@ -36,6 +37,8 @@ func TestRenderChartWithCustomValues(t *testing.T) {
 			return map[string]any{"name": "my-test-cm"}, nil
 		}))
 	require.NoError(t, err)
+	err = o.Write()
+	require.NoError(t, err)
 	assert.Equal(t, "{\"apiVersion\":\"config.kubernetes.io/v1\",\"kind\":\"ResourceList\",\"items\":[{\"apiVersion\":\"v1\",\"data\":{\"input\":\"null\",\"some\":\"value\"},\"kind\":\"ConfigMap\",\"metadata\":{\"name\":\"my-test-cm\"}},{\"apiVersion\":\"somegroup.io/v9001\",\"kind\":\"ATypeNotKnownByTheScheme\",\"metadata\":{\"name\":\"foo\"}}]}\n", output.String())
 }
 
@@ -52,7 +55,8 @@ func TestRenderChartWithHelmHook(t *testing.T) {
 		WithValuesFunc(func(ir *function.InputReader) (map[string]any, error) {
 			return map[string]any{"name": "my-test-cm"}, nil
 		}))
-
+	require.NoError(t, err)
+	err = o.Write()
 	require.NoError(t, err)
 	assert.Equal(t, "{\"apiVersion\":\"config.kubernetes.io/v1\",\"kind\":\"ResourceList\",\"items\":[{\"apiVersion\":\"somegroup.io/v9001\",\"kind\":\"ATypeNotKnownByTheScheme\",\"metadata\":{\"name\":\"foo\"}},{\"apiVersion\":\"v1\",\"data\":{\"some\":\"value\"},\"kind\":\"ConfigMap\",\"metadata\":{\"annotations\":{\"helm.sh/hook\":\"post-install,post-upgrade\",\"helm.sh/hook-delete-policy\":\"before-hook-creation\",\"helm.sh/hook-weight\":\"1\"},\"name\":\"my-test-cm\"}}]}\n", output.String())
 }
