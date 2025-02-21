f4dc2faaa51d5ec1f1b64f4b08bf83c64e6c7ccc
chihshenghuang <pedoe1989@gmail.com>
2025-02-21 09:42:30 -0800

    remove default test

--- pkg/helmshim/helm_test.go
+++ pkg/helmshim/helm_test.go
@@ -2,16 +2,12 @@ package helmshim
 
 import (
 	"bytes"
-	"os"
 	"testing"
 
 	"github.com/Azure/eno/pkg/function"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
-
-// package level stdout for testing without modifying the global stdout
-var stdout = os.Stdout
 
 func TestRenderChart(t *testing.T) {
 	output := bytes.NewBuffer(nil)
@@ -25,37 +21,6 @@ func TestRenderChart(t *testing.T) {
 	err = o.Write()
 	require.NoError(t, err)
 	assert.Equal(t, "{\"apiVersion\":\"config.kubernetes.io/v1\",\"kind\":\"ResourceList\",\"items\":[{\"apiVersion\":\"v1\",\"data\":{\"input\":\"{\\\"apiVersion\\\":\\\"v1\\\",\\\"kind\\\":\\\"ConfigMap\\\",\\\"metadata\\\":{\\\"annotations\\\":{\\\"eno.azure.io/input-key\\\":\\\"foo\\\"},\\\"name\\\":\\\"test-cm\\\"}}\",\"inputResourceName\":\"test-cm\",\"some\":\"value\"},\"kind\":\"ConfigMap\",\"metadata\":{\"name\":null}},{\"apiVersion\":\"somegroup.io/v9001\",\"kind\":\"ATypeNotKnownByTheScheme\",\"metadata\":{\"name\":\"foo\"}}]}\n", output.String())
-}
-
-func TestRenderChartWithDefaultOutputWriter(t *testing.T) {
-	// Save the original stdout and redirect it to a pipe.
-	oldStdout := stdout
-	r, w, _ := os.Pipe()
-	stdout = w
-	// Ensure the original stdout is restored even the test fails.
-	defer func() {
-		stdout = oldStdout
-	}()
-
-	input := bytes.NewBufferString(`{ "items": [{ "apiVersion": "v1", "kind": "ConfigMap", "metadata": { "name": "test-cm", "annotations": { "eno.azure.io/input-key": "foo" } } }] }`)
-	i, err := function.NewInputReader(input)
-	require.NoError(t, err)
-	// Create output writer with the stdout to simulate the default output writer with os.Stdout.
-	o := function.NewOutputWriter(stdout, nil)
-
-	err = RenderChart(WithInputReader(i), WithOutputWriter(o), WithChartPath("fixtures/basic-chart"))
-	require.NoError(t, err)
-	err = o.Write()
-	require.NoError(t, err)
-
-	// Close the writer and capture the output from reader.
-	w.Close()
-	var buf bytes.Buffer
-	buf.ReadFrom(r)
-	output := buf.String()
-
-	// Check the output.
-	assert.Equal(t, "{\"apiVersion\":\"config.kubernetes.io/v1\",\"kind\":\"ResourceList\",\"items\":[{\"apiVersion\":\"v1\",\"data\":{\"input\":\"{\\\"apiVersion\\\":\\\"v1\\\",\\\"kind\\\":\\\"ConfigMap\\\",\\\"metadata\\\":{\\\"annotations\\\":{\\\"eno.azure.io/input-key\\\":\\\"foo\\\"},\\\"name\\\":\\\"test-cm\\\"}}\",\"inputResourceName\":\"test-cm\",\"some\":\"value\"},\"kind\":\"ConfigMap\",\"metadata\":{\"name\":null}},{\"apiVersion\":\"somegroup.io/v9001\",\"kind\":\"ATypeNotKnownByTheScheme\",\"metadata\":{\"name\":\"foo\"}}]}\n", output)
 }
 
 func TestRenderChartWithCustomValues(t *testing.T) {
