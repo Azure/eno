9baa5903c831781356e109518cbabce9c0e4bb86
Jordan Olshevski <jveski@users.noreply.github.com>
2025-04-04 12:31:35 -0500

    Synth integration test helpers (#323)
    Adds `functiontest` - a small package to help test synthesizers written
    in Go.
    
    ---------
    
    Co-authored-by: Jordan Olshevski <joolshev@microsoft.com>

---
+++ pkg/functiontest/testing.go
@@ -0,0 +1,140 @@
+package functiontest
+
+import (
+	"bytes"
+	"fmt"
+	"math/rand/v2"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"sigs.k8s.io/yaml"
+
+	"github.com/Azure/eno/pkg/function"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+// Scenario represents a test case for a synthesizer function.
+type Scenario[T function.Inputs] struct {
+	Name      string
+	Inputs    T
+	Assertion Assertion[T]
+}
+
+// Evaluate runs the synthesizer function with the provided scenarios and asserts on the outputs.
+func Evaluate[T function.Inputs](t *testing.T, synth function.SynthFunc[T], scenarios ...Scenario[T]) {
+	for _, s := range scenarios {
+		t.Run(s.Name, func(t *testing.T) {
+			outputs, err := synth(s.Inputs)
+			if err != nil {
+				t.Fatalf("unexpected error: %v", err)
+			}
+			s.Assertion(t, &s, outputs)
+		})
+	}
+}
+
+// LoadScenarios recursively loads yaml and json input fixtures from the specified directory.
+func LoadScenarios[T any](t *testing.T, dir string, assertion Assertion[T]) []Scenario[T] {
+	scenarios := []Scenario[T]{}
+	walkFiles(t, dir, func(path, name string) error {
+		data, err := os.ReadFile(path)
+		if err != nil {
+			t.Errorf("error while reading fixture %q: %s", path, err)
+			return nil
+		}
+
+		var input T
+		err = yaml.Unmarshal(data, &input)
+		if err != nil {
+			t.Errorf("error while parsing fixture %q: %s", path, err)
+			return nil
+		}
+		scenarios = append(scenarios, Scenario[T]{
+			Name:      name,
+			Inputs:    input,
+			Assertion: assertion,
+		})
+		return nil
+	})
+
+	// Make sure tests aren't coupled to a particular execution order
+	rand.Shuffle(len(scenarios), func(i, j int) { scenarios[i], scenarios[j] = scenarios[j], scenarios[i] })
+
+	return scenarios
+}
+
+type Assertion[T function.Inputs] func(t *testing.T, s *Scenario[T], outputs []client.Object)
+
+// AssertionChain is a helper function to create an assertion that runs multiple assertions in sequence.
+func AssertionChain[T function.Inputs](asserts ...Assertion[T]) Assertion[T] {
+	return func(t *testing.T, s *Scenario[T], outputs []client.Object) {
+		for i, assert := range asserts {
+			t.Run(fmt.Sprintf("assertion-%d", i), func(t *testing.T) {
+				assert(t, s, outputs)
+			})
+		}
+	}
+}
+
+// LoadSnapshots returns an assertion that will compare the outputs of a synthesizer function
+// with the expected outputs stored in snapshot files.
+//
+// Scenarios that do not have a corresponding snapshot file will be ignored.
+// To generate snapshots, set the ENO_GEN_SNAPSHOTS environment variable to a non-empty value.
+//
+// So, to bootstrap snapshots for a given fixture/scenario: create an empty snapshot file
+// that matches the name of the scenario (or fixture if using LoadScenarios), and run the
+// tests with ENO_GEN_SNAPSHOTS=true.
+func LoadSnapshots[T function.Inputs](t *testing.T, dir string) Assertion[T] {
+	snapshots := map[string][]byte{}
+	walkFiles(t, dir, func(path, name string) error {
+		data, err := os.ReadFile(path)
+		if err != nil {
+			t.Errorf("error while reading fixture %q: %s", path, err)
+			return nil
+		}
+		snapshots[name] = data
+		return nil
+	})
+
+	return func(t *testing.T, s *Scenario[T], outputs []client.Object) {
+		expected, ok := snapshots[s.Name]
+		if !ok {
+			return
+		}
+
+		data, err := yaml.Marshal(&outputs)
+		if err != nil {
+			t.Errorf("error while marshalling outputs: %s", err)
+		}
+
+		if os.Getenv("ENO_GEN_SNAPSHOTS") != "" {
+			err = os.WriteFile(filepath.Join(dir, s.Name+".yaml"), data, 0644)
+			if err != nil {
+				t.Errorf("error while writing snapshot %q: %s", s.Name, err)
+			}
+			return
+		}
+
+		if !bytes.Equal(data, expected) {
+			t.Errorf("outputs do not match the snapshot - re-run tests with ENO_GEN_SNAPSHOTS=true to update them")
+		}
+	}
+}
+
+func walkFiles(t *testing.T, dir string, fn func(path, name string) error) {
+	err := filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
+		if err != nil || info.IsDir() {
+			return err
+		}
+		ext := filepath.Ext(info.Name())
+		if ext != ".yaml" && ext != ".yml" && ext != ".json" {
+			return nil
+		}
+		return fn(path, info.Name()[:len(info.Name())-len(ext)])
+	})
+	if err != nil {
+		t.Errorf("error while walking files: %s", err)
+	}
+}
---
+++ pkg/functiontest/testing_test.go
@@ -0,0 +1,89 @@
+package functiontest
+
+import (
+	"sync"
+	"sync/atomic"
+	"testing"
+
+	corev1 "k8s.io/api/core/v1"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+func TestEvaluateBasics(t *testing.T) {
+	fn := func(inputs struct{}) ([]client.Object, error) {
+		output := &corev1.Pod{}
+		output.Name = "test-pod"
+		return []client.Object{output}, nil
+	}
+
+	Evaluate(t, fn, Scenario[struct{}]{
+		Name:   "example-test",
+		Inputs: struct{}{},
+		Assertion: func(t *testing.T, scen *Scenario[struct{}], outputs []client.Object) {
+			if len(outputs) != 1 {
+				t.Errorf("expected 1 output, got %d", len(outputs))
+			}
+		},
+	})
+}
+
+func TestAssertionChain(t *testing.T) {
+	fn := func(inputs struct{}) ([]client.Object, error) {
+		output := &corev1.Pod{}
+		output.Name = "test-pod"
+		return []client.Object{output}, nil
+	}
+
+	calls := atomic.Int64{}
+	Evaluate(t, fn, Scenario[struct{}]{
+		Name:   "example-test",
+		Inputs: struct{}{},
+		Assertion: AssertionChain(
+			func(t *testing.T, scen *Scenario[struct{}], outputs []client.Object) {
+				calls.Add(1)
+			},
+			func(t *testing.T, scen *Scenario[struct{}], outputs []client.Object) {
+				calls.Add(1)
+			},
+		),
+	})
+
+	if calls.Load() != 2 {
+		t.Errorf("expected 2 calls, got %d", calls.Load())
+	}
+}
+
+func TestLoadScenarios(t *testing.T) {
+	fn := func(inputs map[string]any) ([]client.Object, error) {
+		output := &corev1.Pod{}
+		output.Name = "test-pod"
+		return []client.Object{output}, nil
+	}
+
+	var inputs []map[string]any
+	var lock sync.Mutex
+	assertion := func(t *testing.T, scen *Scenario[map[string]any], outputs []client.Object) {
+		lock.Lock()
+		inputs = append(inputs, scen.Inputs)
+		lock.Unlock()
+	}
+
+	scenarios := LoadScenarios(t, "fixtures", assertion)
+	Evaluate(t, fn, scenarios...)
+
+	if len(inputs) != 3 {
+		t.Fatalf("expected 3 inputs, got %d", len(inputs))
+	}
+}
+
+func TestLoadSnapshots(t *testing.T) {
+	fn := func(inputs map[string]any) ([]client.Object, error) {
+		output := &corev1.Pod{}
+		output.Name = "test-pod"
+		return []client.Object{output}, nil
+	}
+
+	assertion := LoadSnapshots[map[string]any](t, "snapshots")
+	scenarios := LoadScenarios(t, "fixtures", assertion)
+	Evaluate(t, fn, scenarios...)
+}
