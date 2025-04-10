7baad4d69a0528b2e1ffd27ed661b454e0f4e243
Jordan Olshevski <joolshev@microsoft.com>
2025-04-10 17:22:40 +0000

    Remove unused metrics

--- internal/controllers/reconciliation/metrics.go
+++ internal/controllers/reconciliation/metrics.go
@@ -14,29 +14,14 @@ var (
 		},
 	)
 
-	resourceVersionChanges = prometheus.NewCounter(
-		prometheus.CounterOpts{
-			Name: "eno_resource_version_cache_miss_total",
-			Help: "Cases where a managed resource's resource version changes outside of Eno",
-		},
-	)
-
 	reconciliationActions = prometheus.NewCounterVec(
 		prometheus.CounterOpts{
 			Name: "eno_reconciliation_actions_total",
 			Help: "Attempts to reconcile managed resources into the desired state, partitioned by action i.e. create, patch, delete",
 		}, []string{"action"},
 	)
-
-	reconciliationScheduleDelta = prometheus.NewHistogram(
-		prometheus.HistogramOpts{
-			Name:    "eno_reconciliation_schedule_delta_seconds",
-			Help:    "Samples time since last reconciliation - the resource's reconcileInterval",
-			Buckets: []float64{0.1, 0.5, 1.0, 5.0, 15.0, 30.0, 60.0},
-		},
-	)
 )
 
 func init() {
-	metrics.Registry.MustRegister(reconciliationLatency, resourceVersionChanges, reconciliationActions, reconciliationScheduleDelta)
+	metrics.Registry.MustRegister(reconciliationLatency, reconciliationActions)
 }
