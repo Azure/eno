b75b486c1eccba7ba3fbbfb3297f2f2ed4b01e0a
Paul Miller <pmiller@microsoft.com>
2025-06-09 16:31:54 +0000

    have example actually use add custom type

--- examples/03-helm-shim/main.go
+++ examples/03-helm-shim/main.go
@@ -3,20 +3,31 @@ package main
 import (
 	"github.com/Azure/eno/pkg/function"
 	"github.com/Azure/eno/pkg/helmshim"
+	corev1 "k8s.io/api/core/v1"
 )
 
-type enoInputs struct{}
+type enoInputs struct {
+	exmpleInput map[string]string `json:"exampleInput"`
+}
+
+func loadConfig(cm *corev1.ConfigMap) (map[string]string, error) {
+	ei := map[string]string{}
+	for k, v := range cm.Data {
+		ei[k] = v
+	}
+	return ei, nil
+}
+
+func init() {
+	function.AddCustomInputType(loadConfig)
+}
 
 func main() {
 	// The Helm shim sets sane defaults, see helmshim.With* for overrides.
 
-	synth := helmshim.Synth(func(enoInputs) (map[string]any, error) {
+	synth := helmshim.Synth(func(ei enoInputs) (map[string]any, error) {
 		return map[string]any{
-			"myinput": map[string]any{
-				"data": map[string]any{
-					"mykey": "myvalue",
-				},
-			},
+			"myinput": ei.exmpleInput,
 		}, nil
 	})
 	function.Main(synth)
