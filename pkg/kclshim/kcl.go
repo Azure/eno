940d4cf0e8e84fba46e743274e311de6e6abf7b5
Lei Yao <yaolei@microsoft.com>
2025-07-01 15:22:14 +1000

    Use go workspace and move README to package godoc

--- pkg/kclshim/kcl.go
+++ pkg/kclshim/kcl.go
@@ -1,3 +1,10 @@
+// Package kclshim allows you to create Eno Synthesizers with KCL (https://www.kcl-lang.io/docs/reference/lang/tour).
+//
+// Here is how to use KCL shim.
+//
+// 1. Create a folder and write your KCLs. Take "./fixtures/example_synthesizer/main.k" as an example.
+//
+// 2. Write a main.go and call "Synthesize()", defined below.
 package kclshim
 
 import (
