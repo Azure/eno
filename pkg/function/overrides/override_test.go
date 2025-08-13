3b4fa9e40ec436b671ba9805f3cc3a15d0269da0
Paul Miller <pmiller@microsoft.com>
2025-08-13 22:34:27 +0000

    make it a subpackage

--- pkg/function/overrides/override_test.go
+++ pkg/function/overrides/override_test.go
@@ -4,7 +4,7 @@ import (
 	"encoding/json"
 	"testing"
 
-	"github.com/Azure/eno/pkg/overrides"
+	"github.com/Azure/eno/pkg/function/overrides"
 
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 )
