a16b689b142ccd19a965673625a75e556f69fe85
Jordan Olshevski <joolshev@microsoft.com>
2025-10-08 22:40:13 +0000

    Fix resource orphaning edge case

--- internal/controllers/reconciliation/controller.go
+++ internal/controllers/reconciliation/controller.go
@@ -209,7 +209,7 @@ func (c *Controller) reconcileSnapshot(ctx context.Context, comp *apiv1.Composit
 	}()
 
 	if res.Deleted() {
-		if current == nil || current.GetDeletionTimestamp() != nil || res.Orphan || (comp.Labels != nil && comp.Labels["eno.azure.io/symphony-deleting"] == "true") {
+		if current == nil || current.GetDeletionTimestamp() != nil || (res.Orphan && comp.DeletionTimestamp != nil) || (comp.Labels != nil && comp.Labels["eno.azure.io/symphony-deleting"] == "true") {
 			return false, nil // already deleted - nothing to do
 		}
 
